c9350cee7f0fcc762b1cfbf1a876c3e5c015e624
Hugo Dominguez <hdominguez@stratio.com>
2020-04-13 11:29:15 +0200

    Change step

--- src/main/java/com/stratio/qa/specs/CCTSpec.java
+++ src/main/java/com/stratio/qa/specs/CCTSpec.java
@@ -955,6 +955,7 @@ public class CCTSpec extends BaseGSpec {
     /**
      * Install service
      * @param service   service name
+     * @param folder    folder where service are going to be installed
      * @param model     service model
      * @param version   service version
      * @param name      service instance name
@@ -962,8 +963,8 @@ public class CCTSpec extends BaseGSpec {
      * @param jsonFile  marathon json to deploy
      * @throws Exception
      */
-    @Given("^I install service '(.+?)' with model '(.+?)' and version '(.+?)' and instance name '(.+?)' in tenant '(.+?)' using json '(.+?)'$")
-    public void installServiceFromMarathonJson(String service, String model, String version, String name, String tenant, String jsonFile) throws Exception {
+    @Given("^I install service '(.+?)'( in folder '(.+?)')? with model '(.+?)' and version '(.+?)' and instance name '(.+?)' in tenant '(.+?)' using json '(.+?)'$")
+    public void installServiceFromMarathonJson(String service, String folder, String model, String version, String name, String tenant, String jsonFile) throws Exception {
         String endPoint = "/service/" + ThreadProperty.get("deploy_api_id") + "/deploy/" + service + "/" + model + "/" + version + "/schema?tenantId=" + tenant;
         String data = this.commonspec.retrieveData(jsonFile, "json");
 
@@ -986,24 +987,13 @@ public class CCTSpec extends BaseGSpec {
         }
 
         String serviceName = "/" + name;
-
+        if (folder != null) {
+            serviceName = "/" + folder + "/" + name;
+        }
         if (!"NONE".equals(tenant)) {
-            switch (service) {
-                case "search-engine":
-                    serviceName = "/" + tenant + "/search-engine/" + tenant + "-" + name;
-                    break;
-                default:
-                    serviceName = "/" + tenant + "/" + tenant + "-" + name;
-                    break;
-            }
-        } else {
-            switch (service) {
-                case "search-engine":
-                    serviceName = "/search-engine/" + name;
-                    break;
-                default:
-                    serviceName = "/" + name;
-                    break;
+            serviceName = "/" + tenant + "/" + tenant + "-" + name;
+            if (folder != null) {
+                serviceName =  "/" + tenant + "/" + folder + "/" + tenant + "-" + name;
             }
         }
 
